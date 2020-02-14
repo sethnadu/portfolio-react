3a8a5b65032d98e57478081951fdde1ffd2d8ba2
Seth Nadu <seth.nadu@gmail.com>
2020-02-14 12:20:25 -0500

    fixed background color issue

--- src/Components/header/HeaderDrawer.jsx
+++ src/Components/header/HeaderDrawer.jsx
@@ -95,7 +95,7 @@ const useStyles = makeStyles(theme => ({
   content: {
     width: "100%",
     height: '100%',
-    backgroundColor: "#393f4d",
+    backgroundColor: "#F5F5F5",
 
   },
   contentOpen: {
